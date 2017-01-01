94319db7c0ceaf901d10d3c4290667c8a1425c2d
Your Name <you@example.com>
2017-01-01 20:40:14 +0300

    First Commit

---
+++ afisha_api.py
@@ -0,0 +1,84 @@
+import requests
+import random
+import string
+import pyDes as coding
+
+
+class afisha:
+    def schedule_cinema(self):
+        self.action = 'theme_schedule'
+        query = {'action': self.action, 'cityId': self.city_id, 'themeid': 2,
+                 'start': self.start, 'count': self.count,
+                 'sortOrder': self.sort}
+        return self.request(query)['list']
+
+
+    def film_review(self, film_id):
+        self.action = 'review_list'
+        query = {'action': self.action, 'clsId': self.cls_id, 'id': film_id,
+                 'count': self.count, 'start': self.start}
+        return self.request(query)
+
+
+    def film_photos(self, film_id):
+        self.action = 'image_list'
+        query = {'action': self.action, 'clsId': self.cls_id,
+                 'count': self.count, 'start': self.start,
+                 'iscrop': 0, 'id': film_id}
+        return self.request(query)
+
+
+    def film_info(self, film_id):
+        self.action = 'product'
+        query = {'action': self.action, 'clsId': self.cls_id,
+                 'cityId': self.city_id, 'id': film_id}
+        return self.request(query)[0]
+
+
+    def film_schedule(self, film_id):
+        self.action = 'schedule'
+        query = {'action': self.action, 'clsId': self.cls_id, 'id': film_id,
+                 'cityId': self.city_id, 'scheduleFormat': 1,
+                 'sortOrder': self.sort}
+        return self.request(query)
+
+
+    def request(self, query):
+        return requests.post(self.api_url, params=self.session_payload,
+                             data=query).json()
+
+
+    def __init__(self):
+        SECRET = bytes([45, 56, 38, 71, 42, 105, 50, 77])
+        INT_2_POW_64 = 18446744073709551616
+        self.api_url = 'http://api.afisha.ru/mobile/Service.aspx'
+        self.city_id = '2'
+        self.cls_id = '16'
+        self.sort = 5
+        self.start = '0'
+        self.count = '1000'
+        self.phone_id = ''.join(random.sample(string.ascii_lowercase, 12))
+        self.phone_name = random.choice(('JiaYu', 'Acer', 'Samsung', 'Fly',))
+        self.phone_resolution = random.choice(('480x800', '720x1280',))
+        self.platform_ver = random.choice(('4.2.2', '4.4', '5.0', '5.1',))
+        self.session = '-1'
+        self.user_info = {'X-Afisha-Phone-Platform': 'Android',
+                          'X-Afisha-Session-Id': self.session,
+                          'X-Afisha-App-Name': 'Android-Afisha',
+                          'X-Afisha-App-Ver': '2.3.13',
+                          'X-Afisha-Protocol-Ver': '1.3',
+                          'X-Afisha-Phone-Id': self.phone_id,
+                          'X-Afisha-Phone-Name': self.phone_name,
+                          'X-Afisha-Phone-Resolution': self.phone_resolution,
+                          'X-Afisha-Phone-Platform-Version': self.platform_ver}
+        responce = requests.post(self.api_url, headers=self.user_info)
+        session_request = responce.headers['X-Afisha-Session-Request']
+        req_bytes = int(session_request).to_bytes(8, byteorder='little')
+        cipher = coding.des(SECRET, coding.ECB, pad=None,
+                            padmode=coding.PAD_NORMAL)
+        byte_array = cipher.encrypt(req_bytes)
+        session = int.from_bytes(byte_array, byteorder='little', signed=True)
+        if session < 0:
+            session += INT_2_POW_64
+        self.session_payload = {'X-Afisha-Phone-Id': self.phone_id,
+                                'X-Afisha-Session-Id': session}
--- server.py
+++ server.py
@@ -1,10 +1,54 @@
+from afisha_api import afisha
 from flask import Flask, render_template
+from werkzeug.contrib.cache import SimpleCache
+
 
 app = Flask(__name__)
+cache = SimpleCache()
+api = afisha()
+
 
 @app.route('/')
-def films_list():
-    return render_template('films_list.html')
+def show_films():
+    films_dict = cache.get('films')
+    if films_dict is None:
+        films_dict = {}
+        films_list = api.schedule_cinema()
+        for film in films_list:
+            id = film.get('id')
+            films_dict[id] = api.film_info(id)
+            films_dict[id]['rate_int'] = int(films_dict[id]['rate'])
+            schedule = api.film_schedule(id)
+            films_dict[id]['cinema_count'] = schedule['maxCount']
+            films_dict[id]['schedule'] = schedule['list']
+            review = api.film_review(id)
+            films_dict[id]['review_count'] = review['maxCount']
+            films_dict[id]['review'] = review['list']
+            photos = api.film_photos(id)
+            films_dict[id]['photos_count'] = photos['maxCount']
+            films_dict[id]['photos'] = photos['list']
+            films_dict[id]['id'] = id
+        cache.set('films', films_dict, 12*60*60)
+    sorted_films = sorted(films_dict.items(), reverse=True,
+                              key=lambda x: x[1]['cinema_count'])
+    return render_template('films_list.html', films=enumerate(sorted_films))
 
-if __name__ == "__main__":
-    app.run()
+
+@app.route('/film/<int:film_id>')
+def show_film_info(film_id):
+    films = cache.get('films')
+    film_info = films.get(film_id)
+    return render_template('film_info.html', film=film_info)
+
+
+@app.route('/film/content/<int:film_id>')
+def show_film_content(film_id):
+    films = cache.get('films')
+    film_info = films.get(film_id)
+    return render_template('film_content.html',
+                           content=(enumerate(film_info['trailers']),
+                                    enumerate(film_info['photos'])))
+
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', debug=True)
