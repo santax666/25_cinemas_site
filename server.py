0f484028345dbabbd9e049af980c7325cb2462e6
Your Name <you@example.com>
2017-01-08 21:43:10 +0300

    Внесены правки в шаблоны для корректного форматирования страниц

--- server.py
+++ server.py
@@ -11,14 +11,13 @@ api = afisha()
 @app.route('/')
 @app.route('/page/<int:page_id>')
 def show_films(page_id=1):
-    films_dict = {}
     films = api.schedule_cinema((page_id-1)*9)
     films_pages = (films['maxCount']-1) // 9 + 1
     for film in films['list']:
         film.update(api.film_info(film['id']))
         film['rate_int'] = int(film['rate'])
         cache.set(film['id'], film, 12*60*60)
-    return render_template('films.html', films=enumerate(films),
+    return render_template('films.html', films=enumerate(films['list']),
                            pages=(films_pages, page_id,))
 
 
